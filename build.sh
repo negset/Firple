0a4e799422f8f7fea69c7730f177ac36b6b07a0e
negset <15167516+negset@users.noreply.github.com>
2021-07-30 19:35:22 +0900

    image branch

--- build.sh
+++
@@ -1,9 +0,0 @@
-fontforge -script generate.py \
-    'Regular' \
-    'fonts/IBMPlexSansJP-Text.ttf' \
-    'fonts/FiraCode-Regular.ttf'
-
-fontforge -script generate.py \
-    'Bold' \
-    'fonts/IBMPlexSansJP-Bold.ttf' \
-    'fonts/FiraCode-Bold.ttf'
