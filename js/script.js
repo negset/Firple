ae0a535807a2b62123e4827db740f7837bd770d3
negset <15167516+negset@users.noreply.github.com>
2021-12-23 00:30:22 +0900

    support firple italic

--- js/script.js
+++ js/script.js
@@ -6,31 +6,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const navbarBurger = document.querySelector('.navbar-burger');
   const burgerTarget = document.getElementById(navbarBurger.dataset.target);
-  navbarBurger.addEventListener('click', () => {
+  navbarBurger.onclick = () => {
     navbarBurger.classList.toggle('is-active');
     burgerTarget.classList.toggle('is-active');
-  });
-  main.addEventListener('click', () => {
+  };
+  main.onclick = () => {
     navbarBurger.classList.remove('is-active');
     burgerTarget.classList.remove('is-active');
-  });
+  };
 
   const textArea = document.querySelector('.sample-text');
-  const regularBtn = document.getElementById('regular');
-  regularBtn.addEventListener('input', () => {
-    textArea.style.fontWeight = 400;
-  });
-  const boldBtn = document.getElementById('bold');
-  boldBtn.addEventListener('input', () => {
-    textArea.style.fontWeight = 700;
-  });
-  const weightSldr = document.getElementById('weight');
-  weightSldr.value = parseInt(window.getComputedStyle(textArea)['fontSize']);
-  weightSldr.addEventListener('input', () => {
-    textArea.style.fontSize = weightSldr.value + 'px';
-  });
-  const ligatureChk = document.getElementById('ligature');
-  ligatureChk.addEventListener('input', () => {
-    textArea.style.fontVariantLigatures = ligatureChk.checked ? "normal" : "none";
-  });
+  const subfamily = document.getElementById('subfamily');
+  subfamily.onchange = e => {
+    const v = e.target.value;
+    textArea.style.fontStyle = (v == 'regular' || v == 'bold') ? 'normal' : 'italic';
+    textArea.style.fontWeight = (v == 'regular' || v == 'italic') ? 400 : 700;
+  };
+  const weight = document.getElementById('weight');
+  weight.value = parseInt(window.getComputedStyle(textArea)['fontSize']);
+  weight.oninput = e => {
+    textArea.style.fontSize = e.target.value + 'px';
+  };
+  const ligature = document.getElementById('ligature');
+  ligature.oninput = e => {
+    textArea.style.fontVariantLigatures = e.target.checked ? "normal" : "none";
+  };
 });
