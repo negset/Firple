714195a8428865426faa777c1e74987bcf2c17d3
negset <15167516+negset@users.noreply.github.com>
2023-04-24 17:41:33 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('main');
-  const navHeight = window.getComputedStyle(document.querySelector('nav'))['height'];
-  const footerHeight = window.getComputedStyle(document.querySelector('footer'))['height'];
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.querySelector("main");
+  const navHeight = window.getComputedStyle(document.querySelector("nav"))["height"];
+  const footerHeight = window.getComputedStyle(document.querySelector("footer"))["height"];
   main.style.minHeight = `calc(100vh - ${navHeight} - ${footerHeight})`;
 
-  const navbarBurger = document.querySelector('.navbar-burger');
+  const navbarBurger = document.querySelector(".navbar-burger");
   const burgerTarget = document.getElementById(navbarBurger.dataset.target);
   navbarBurger.onclick = () => {
-    navbarBurger.classList.toggle('is-active');
-    burgerTarget.classList.toggle('is-active');
+    navbarBurger.classList.toggle("is-active");
+    burgerTarget.classList.toggle("is-active");
   };
   main.onclick = () => {
-    navbarBurger.classList.remove('is-active');
-    burgerTarget.classList.remove('is-active');
+    navbarBurger.classList.remove("is-active");
+    burgerTarget.classList.remove("is-active");
   };
 
-  const textArea = document.querySelector('.sample-text');
-  const subfamily = document.getElementById('subfamily');
-  subfamily.onchange = e => {
+  const textArea = document.querySelector(".sample-text");
+  document.getElementById("subfamily").onchange = e => {
     const v = e.target.value;
-    textArea.style.fontStyle = (v == 'regular' || v == 'bold') ? 'normal' : 'italic';
-    textArea.style.fontWeight = (v == 'regular' || v == 'italic') ? 400 : 700;
+    textArea.style.fontFamily = (v == "firple") ? "Firple" : "Firple Slim";
   };
-  const weight = document.getElementById('weight');
-  weight.value = parseInt(window.getComputedStyle(textArea)['fontSize']);
+  document.getElementById("bold").oninput = e => {
+    textArea.style.fontWeight = e.target.checked ? 700 : 400;
+  };
+  document.getElementById("italic").oninput = e => {
+    textArea.style.fontStyle = e.target.checked ? "italic" : "normal";
+  };
+  const weight = document.getElementById("weight");
+  weight.value = parseInt(window.getComputedStyle(textArea)["fontSize"]);
   weight.oninput = e => {
-    textArea.style.fontSize = e.target.value + 'px';
+    textArea.style.fontSize = e.target.value + "px";
   };
-  const ligature = document.getElementById('ligature');
-  ligature.oninput = e => {
+  document.getElementById("ligature").oninput = e => {
     textArea.style.fontVariantLigatures = e.target.checked ? "normal" : "none";
   };
 });
