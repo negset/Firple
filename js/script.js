f135ba66796200d5e564749d1070c16ab1ea81f3
negset <15167516+negset@users.noreply.github.com>
2024-10-09 00:08:19 +0900

    update to firple 5.000

--- js/script.js
+++ js/script.js
@@ -20,18 +20,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const v = e.target.value;
     textArea.style.fontFamily = (v == "firple") ? "Firple" : "Firple Slim";
   };
+  const weight = document.getElementById("weight");
+  const weightText = document.getElementById("weight-text");
+  weight.value = parseInt(window.getComputedStyle(textArea)["fontSize"]);
+  weightText.innerHTML = window.getComputedStyle(textArea)["fontSize"];
+  weight.oninput = e => {
+    textArea.style.fontSize = e.target.value + "px";
+    weightText.innerHTML = e.target.value + "px";
+  };
   document.getElementById("bold").oninput = e => {
     textArea.style.fontWeight = e.target.checked ? 700 : 400;
   };
   document.getElementById("italic").oninput = e => {
     textArea.style.fontStyle = e.target.checked ? "italic" : "normal";
   };
-  const weight = document.getElementById("weight");
-  weight.value = parseInt(window.getComputedStyle(textArea)["fontSize"]);
-  weight.oninput = e => {
-    textArea.style.fontSize = e.target.value + "px";
-  };
   document.getElementById("ligature").oninput = e => {
     textArea.style.fontVariantLigatures = e.target.checked ? "normal" : "none";
   };
+  const cv33 = document.getElementById("cv33");
+  const ss11 = document.getElementById("ss11");
+  featureOnInput = e => {
+    if (cv33.checked && ss11.checked)
+      textArea.style.fontFeatureSettings = "'cv33', 'ss11'";
+    else if (cv33.checked)
+      textArea.style.fontFeatureSettings = "'cv33'";
+    else if (ss11.checked)
+      textArea.style.fontFeatureSettings = "'ss11'";
+    else
+      textArea.style.fontFeatureSettings = "";
+  };
+  cv33.oninput = featureOnInput;
+  ss11.oninput = featureOnInput;
 });
