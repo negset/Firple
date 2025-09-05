97dd2b9a5c4fa2a768c7dd6f7f96456a5c407356
negset <15167516+negset@users.noreply.github.com>
2025-09-06 02:35:13 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // main min-height
   const main = document.querySelector("main");
-  const navHeight = window.getComputedStyle(document.querySelector("nav"))["height"];
-  const footerHeight = window.getComputedStyle(document.querySelector("footer"))["height"];
+  const navHeight = getComputedStyle(document.querySelector("nav"))["height"];
+  const footerHeight = getComputedStyle(document.querySelector("footer"))["height"];
   main.style.minHeight = `calc(100vh - ${navHeight} - ${footerHeight})`;
 
+  // navbar burger
   const navbarBurger = document.querySelector(".navbar-burger");
   const burgerTarget = document.getElementById(navbarBurger.dataset.target);
   navbarBurger.onclick = () => {
@@ -15,40 +17,66 @@ document.addEventListener("DOMContentLoaded", () => {
     burgerTarget.classList.remove("is-active");
   };
 
-  const textArea = document.querySelector(".preview-text");
-  document.getElementById("subfamily").onchange = e => {
+  // preview config
+  const previewConfig = document.querySelector("#preview-config");
+  const textArea = document.querySelector("#preview-text");
+  document.querySelector("#subfamily").onchange = e => {
     const v = e.target.value;
     textArea.style.fontFamily = (v == "firple") ? "Firple" : "Firple Slim";
   };
-  const weight = document.getElementById("weight");
-  const weightText = document.getElementById("weight-text");
-  weight.value = parseInt(window.getComputedStyle(textArea)["fontSize"]);
-  weightText.innerHTML = window.getComputedStyle(textArea)["fontSize"];
-  weight.oninput = e => {
+  const boldButton = document.querySelector("#bold-button");
+  boldButton.onclick = () => {
+    let active = boldButton.classList.toggle("is-active");
+    textArea.style.fontWeight = active ? 700 : 400;
+  };
+  const italicButton = document.querySelector("#italic-button");
+  italicButton.onclick = () => {
+    let active = italicButton.classList.toggle("is-active");
+    textArea.style.fontStyle = active ? "italic" : "normal";
+  };
+  const weightSlider = document.querySelector("#weight-slider");
+  const weightText = document.querySelector("#weight-text");
+  weightSlider.value = parseInt(getComputedStyle(textArea)["fontSize"]);
+  weightText.innerHTML = getComputedStyle(textArea)["fontSize"];
+  weightSlider.oninput = e => {
     textArea.style.fontSize = e.target.value + "px";
     weightText.innerHTML = e.target.value + "px";
   };
-  document.getElementById("bold").oninput = e => {
-    textArea.style.fontWeight = e.target.checked ? 700 : 400;
-  };
-  document.getElementById("italic").oninput = e => {
-    textArea.style.fontStyle = e.target.checked ? "italic" : "normal";
-  };
-  document.getElementById("ligature").oninput = e => {
+  document.querySelector("#ligature-checkbox").oninput = e => {
     textArea.style.fontVariantLigatures = e.target.checked ? "normal" : "none";
   };
-  const cv33 = document.getElementById("cv33");
-  const ss11 = document.getElementById("ss11");
-  featureOnInput = e => {
-    if (cv33.checked && ss11.checked)
-      textArea.style.fontFeatureSettings = "'cv33', 'ss11'";
-    else if (cv33.checked)
-      textArea.style.fontFeatureSettings = "'cv33'";
-    else if (ss11.checked)
-      textArea.style.fontFeatureSettings = "'ss11'";
-    else
-      textArea.style.fontFeatureSettings = "";
+  // features
+  const features = {
+    cv33: false,
+    ss11: false,
   };
-  cv33.oninput = featureOnInput;
-  ss11.oninput = featureOnInput;
+  function toggleFeature(tag) {
+    features[tag] = !features[tag];
+    textArea.style.fontFeatureSettings = Object.entries(features)
+      .map(([tag, enabled]) => `"${tag}" ${enabled ? 1 : 0}`)
+      .join(", ");
+  }
+  document.querySelectorAll(".feature-checkbox").forEach(e => {
+    e.oninput = () => toggleFeature(e.dataset.tag);
+  });
+
+  // font loading
+  document.fonts.ready.then(() => {
+    previewConfig.disabled = false;
+    textArea.value =
+      `彼らの機器や装置はすべて生命体だ。
+Almost before we knew it, we had left the ground.
+
+!=->>++:=`
+  });
+  document.fonts.onloading = () => {
+    previewConfig.disabled = true;
+    let currentText = textArea.value;
+    textArea.value = "読み込み中...";
+
+    document.fonts.onloadingdone = () => {
+      previewConfig.disabled = false;
+      textArea.value = currentText;
+    };
+  };
 });
